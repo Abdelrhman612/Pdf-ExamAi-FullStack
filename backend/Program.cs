83def4c00e4822dd831a09d85949c2e417004cb1
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2025-12-28 19:37:58 +0200

    update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,6 @@
 using backend.data;
+using backend.InterFaces;
+using backend.Repository;
 using backend.Services;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,7 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
-builder.Services.AddHttpClient<FastApiService>();
+
+builder.Services.AddHttpClient<IFastApiService , FastApiService >();
+builder.Services.AddScoped<IPdfRepository, PdfRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
