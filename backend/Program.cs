85f0f9651fa1bd1425fdae87eb475b1d21b586f5
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2026-01-05 14:34:18 +0200

    update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,12 +5,22 @@ using backend.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var allowedOrigins = builder.Configuration.GetSection("allowedOrigins").Get<string[]>();
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
-builder.Services.AddHttpClient<IFastApiService , FastApiService >();
+builder.Services.AddCors(options =>
+{
+    if (allowedOrigins != null && allowedOrigins.Length > 0)
+    {
+        options.AddPolicy("React", builder => builder.WithOrigins(allowedOrigins).AllowAnyMethod().AllowAnyHeader());
+    }
+});
+
+builder.Services.AddHttpClient<IFastApiService, FastApiService>();
 builder.Services.AddScoped<IPdfRepository, PdfRepository>();
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -29,6 +39,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapControllers();
+app.UseCors("React");
 app.MapGet("/", () => "Server is running...");
 app.Run();
 
