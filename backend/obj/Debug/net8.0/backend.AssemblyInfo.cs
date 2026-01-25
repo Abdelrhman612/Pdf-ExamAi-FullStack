ed02207a739bf96a881772a2ca57548b4a7df591
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2026-01-25 14:24:41 +0200

    update backend.AssemblyInfo.cs and backend.AssemblyInfoInputs.cache

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f827f4763d5daa58de8c6fc11fb96883cd61fb11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d3c85cd9e94fcac6674d9f4c7d36b7fff8ac5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
