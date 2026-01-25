cbeb8c3b1df6b5203c9bdd6e0cbe6fde6b3a7c99
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2026-01-25 14:17:42 +0200

    update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,40 +6,52 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddDbContext<AppDbContext>(op =>
+    op.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+);
 
 var allowedOrigins = builder.Configuration.GetSection("allowedOrigins").Get<string[]>();
+
 builder.Services.AddCors(options =>
 {
-    if (allowedOrigins != null && allowedOrigins.Length > 0)
+    options.AddPolicy("ReactPolicy", policy =>
     {
-        options.AddPolicy("ReactPolicy", policy => policy.WithOrigins(allowedOrigins).AllowAnyMethod().AllowAnyHeader().AllowCredentials());
-    }
+        if (allowedOrigins != null && allowedOrigins.Length > 0)
+        {
+            policy.WithOrigins(allowedOrigins)
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowCredentials();
+        }
+        else
+        {
+
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        }
+    });
 });
+
 
 builder.Services.AddHttpClient<IFastApiService, FastApiService>();
 builder.Services.AddScoped<IPdfRepository, PdfRepository>();
 
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseRouting();
+
 app.UseCors("ReactPolicy");
-app.UseHttpsRedirection();
-app.UseAuthentication();
+
+// app.UseHttpsRedirection(); 
+
 app.UseAuthorization();
+
 app.MapGet("/", () => "Server is running...");
 app.MapControllers();
-app.Run();
 
+app.Run();
